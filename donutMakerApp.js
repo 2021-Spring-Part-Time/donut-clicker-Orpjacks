b1daa7a67069e504d8ab4b62e3faedf428c54de8
Orpjacks <orpjacks@gmail.com>
2021-07-08 11:36:28 -0400

    git init

---
+++ donutMakerApp.js
@@ -0,0 +1,20 @@
+// import DonutMaker from'/DonutMaker';
+
+// const createBtn1 = document.querySelector('.donutclicker'),
+// donatClicker = document.querySelector('.donutclicker')
+
+// createBtn1.addEventListener('click', ()=>{
+    
+// })
+
+// const donutcount = document.createElement('p');
+// donutcount.innerText = 0 + '' + "Donuts";
+// donutClicker.appendChild(donutcount);
+
+
+// const createBtn2 = document.querySelector('.autoclickers');
+
+// const donutcount = document.createElement('p');
+// donutcount.innerText = + '' + "Donuts";
+
+
